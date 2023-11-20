95c8a16d143ad1cf3741e0e320a3a2572b5fd748
soyebsuvo <soyebbeen@gmail.com>
2023-11-21 01:08:39 +0600

    patch api created

--- index.js
+++ index.js
@@ -34,6 +34,18 @@ async function run() {
       res.send(result);
     })
 
+    app.patch("/users/admin/:id" , async (req , res) => {
+      const id = req.params.id;
+      const filter = { _id : new ObjectId(id)};
+      const updatedDoc = {
+        $set:{
+          role : "admin"
+        }
+      }
+      const result = await usersCollection.updateOne(filter , updatedDoc);
+      res.send(result)
+    })
+
     app.delete("/users/:id" , async ( req , res ) => {
       const id = req.params.id;
       const query = { _id : new ObjectId(id)};
